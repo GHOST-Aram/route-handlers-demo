e3e18cae6ac47546c68ce46e369702270dfbbc59
GHOST-Aram <owinoojala@gmail.com>
2024-02-16 17:46:15 +0300

    chore: Explore request headers

--- src/app/dashboard/users/route.ts
+++ src/app/dashboard/users/route.ts
@@ -1,3 +1,13 @@
-export const GET = async() =>{
-    return new Response("Users Data")
+import { NextRequest } from "next/server"
+import { headers } from "next/headers"
+export const GET = async(request: NextRequest) =>{
+
+    const reqHeaders = new Headers(request.headers)
+    const headerList = headers()
+
+    return Response.json( {
+        data: "Users Data", 
+        authorization1: reqHeaders.get("Authorization"), 
+        authorization2: headerList.get("Authorization")
+    } )
 }
