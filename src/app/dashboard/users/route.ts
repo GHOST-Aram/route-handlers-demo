d978f9ec733777ba6210698bc290031fecfddbe3
GHOST-Aram <owinoojala@gmail.com>
2024-02-16 17:58:18 +0300

    chore: Explore setting and getting cookies

--- src/app/dashboard/users/route.ts
+++ src/app/dashboard/users/route.ts
@@ -5,9 +5,15 @@ export const GET = async(request: NextRequest) =>{
     const reqHeaders = new Headers(request.headers)
     const headerList = headers()
 
-    return Response.json( {
+    return new Response ( JSON.stringify({
         data: "Users Data", 
         authorization1: reqHeaders.get("Authorization"), 
-        authorization2: headerList.get("Authorization")
+        authorization2: headerList.get("Authorization"),
+        cookie: request.cookies.get("theme")
+    }), {
+        headers:{
+            "Content-Type": "application/json",
+            "Cookie": "theme=dark"
+        }
     } )
 }
