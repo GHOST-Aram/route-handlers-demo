e94b67d72858bea7adc9850b048832ec7ff96119
GHOST-Aram <owinoojala@gmail.com>
2024-02-18 18:11:18 +0300

    chore: Try Input validation with Joi

--- src/app/dashboard/route.ts
+++ src/app/dashboard/route.ts
@@ -1,3 +1,33 @@
-export const GET = async() =>{
-    return new Response("Dashboard data")
+import Joi from "joi"
+import { NextRequest, NextResponse } from "next/server"
+export const POST = async(request: NextRequest) =>{
+
+    const userData = await request.json()
+    const userSchema = Joi.object({
+        name: Joi.string().min(2).max(100),
+        email: Joi.string().email(),
+        password: Joi.string().alphanum(),
+        // role: Joi.string()
+    })
+
+    try {
+        
+        const {value, error} = await userSchema.validateAsync(userData)
+
+        return new NextResponse(JSON.stringify(userData), {
+            status: 201,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return new NextResponse(JSON.stringify(error), {
+            status: 400,
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
 }
